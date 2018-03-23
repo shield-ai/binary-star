6700a17d0727fae6509298fb33305e4940be0548
s-stanko <sean.stanko@shield.ai>
2018-03-22 20:53:11 -0700

    Only include rosbag stuff.

--- ros_comm/debit.sh
+++ ros_comm/debit.sh
@@ -5,7 +5,8 @@ set -e
 sudo apt install python-bloom fakeroot dpkg-dev debhelper
 
 # Get all directories that have code to build.
-PACKAGES=$(find . -name package.xml -exec bash -c 'test -f $(dirname "$1")/CMakeLists.txt' -- {} \; -print | grep -v test | xargs dirname)
+#PACKAGES=$(find . -name package.xml -exec bash -c 'test -f $(dirname "$1")/CMakeLists.txt' -- {} \; -print | grep -v test | xargs dirname)
+PACKAGES="ros_comm/tools/rosbag ros_comm/tools/rosbag_storage"
 
 for PACKAGE in $PACKAGES; do
   pushd "$PACKAGE" >> /dev/null
